1d0861cbf88be309a8ff05cb1de332f6ed8be1f5
morningwn <morningwn@126.com>
2023-11-24 10:24:48 +0800

    2651. 计算列车到站时间

--- src/main/java/com/github/morningwn/t2501_t3000/t2601_t2700/t2651_t2660/J2651.java
+++ src/main/java/com/github/morningwn/t2501_t3000/t2601_t2700/t2651_t2660/J2651.java
@@ -1,4 +1,4 @@
-package com.github.morningwn.t2501_.t2601_t2700.t2651_t2660;
+package com.github.morningwn.t2501_t3000.t2601_t2700.t2651_t2660;
 
 /**
  * <a href="https://leetcode.cn/problems/calculate-delayed-arrival-time/">2651. 计算列车到站时间</a>
@@ -6,7 +6,7 @@ package com.github.morningwn.t2501_.t2601_t2700.t2651_t2660;
  * @author morningwn
  * @date Created in 2023/9/8 22:06
  */
-public class T2651 {
+public class J2651 {
 
     public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
         return (arrivalTime + delayedTime) % 24;
