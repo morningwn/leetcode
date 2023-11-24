751120cffb626873ea565c5b1b10e8c6ed7194fb
morningwn <morningwn@126.com>
2023-11-24 10:21:23 +0800

    2656. K 个元素的最大和

--- src/main/java/com/github/morningwn/t2501_t3000/t2501_t2600/t2651_t2660/J2656.java
+++ src/main/java/com/github/morningwn/t2501_t3000/t2501_t2600/t2651_t2660/J2656.java
@@ -1,4 +1,4 @@
-package com.github.morningwn.t2501_.t2501_t2600.t2651_t2660;
+package com.github.morningwn.t2501_t3000.t2501_t2600.t2651_t2660;
 
 /**
  * <a href="https://leetcode.cn/problems/maximum-sum-with-exactly-k-elements/">2656. K 个元素的最大和</a>
@@ -6,7 +6,7 @@ package com.github.morningwn.t2501_.t2501_t2600.t2651_t2660;
  * @author morningwn
  * @date 2023/11/15 10:33
  */
-public class T2656 {
+public class J2656 {
     public int maximizeSum(int[] nums, int k) {
         int maxValue = Integer.MIN_VALUE;
         for (int num : nums) {
