55a4bbfc0b5069f4e836de4dc970276a1452ff5f
morningwn <morningwn@126.com>
2023-11-30 16:57:04 +0800

    1816. 截断句子

--- src/main/java/com/github/morningwn/create/GetById.java
+++ src/main/java/com/github/morningwn/create/GetById.java
@@ -3,14 +3,14 @@ package com.github.morningwn.create;
 import java.util.List;
 
 /**
- * @author wangjianing
+ * @author morningwn
  * @date 2023/11/27 11:58
  */
 public class GetById {
 
     public static void main(String[] args) {
 
-        int id = 904;
+        int id = 1653;
         List<String> list = Main.getList(id - 1, id + 50);
         try {
             Main.makeFile(Main.getDetail(list.get(0)));
