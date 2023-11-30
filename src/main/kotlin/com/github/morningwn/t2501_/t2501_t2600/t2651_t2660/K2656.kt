57bf1952fa70a0f2ba6de7823986b700e2fda025
morningwn <morningwn@126.com>
2023-11-30 16:57:15 +0800

    2656. K 个元素的最大和

--- src/main/kotlin/com/github/morningwn/t2501_/t2501_t2600/t2651_t2660/K2656.kt
+++ src/main/kotlin/com/github/morningwn/t2501_/t2501_t2600/t2651_t2660/K2656.kt
@@ -3,7 +3,7 @@ package com.github.morningwn.t2501_.t2501_t2600.t2651_t2660
 /**
  * <a href="https://leetcode.cn/problems/maximum-sum-with-exactly-k-elements/">2656. K 个元素的最大和</a>
  *
- * @author wangjianing
+ * @author morningwn
  * @date 2023/11/15 10:35
  */
 class K2656 {
