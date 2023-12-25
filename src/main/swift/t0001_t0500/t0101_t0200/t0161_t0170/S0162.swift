341839212202ab67b0fb51bea3b53eaab6369d25
morningwn <morningwn@126.com>
2023-12-25 12:05:08 +0800

    162. 寻找峰值

--- src/main/swift/t0001_t0500/t0101_t0200/t0161_t0170/S0162.swift
+++ src/main/swift/t0001_t0500/t0101_t0200/t0161_t0170/S0162.swift
@@ -6,23 +6,25 @@
 
 import Foundation
 
-func findPeakElement(_ nums: [Int]) -> Int {
-    if nums.count == 1 {
-        return 0
-    }
-    if nums[0] > nums[1] {
-        return 0
-    }
-
-    for i in 1..<nums.count - 1 {
-        if nums[i - 1] < nums[i] && nums[i] < nums[i + 1] {
-            return i
+class S0162 {
+    func findPeakElement(_ nums: [Int]) -> Int {
+        if nums.count == 1 {
+            return 0
         }
-    }
+        if nums[0] > nums[1] {
+            return 0
+        }
 
-    if nums[nums.count - 1] > nums[nums.count - 2] {
-        return nums.count - 1
-    }
+        for i in 1..<nums.count - 1 {
+            if nums[i - 1] < nums[i] && nums[i] < nums[i + 1] {
+                return i
+            }
+        }
 
-    return -1
+        if nums[nums.count - 1] > nums[nums.count - 2] {
+            return nums.count - 1
+        }
+
+        return -1
+    }
 }
