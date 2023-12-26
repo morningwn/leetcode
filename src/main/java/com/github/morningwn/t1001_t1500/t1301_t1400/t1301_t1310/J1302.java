b1eea22a18290d3cd5b6619602943cf3f88f79b8
morningwn <morningwn@126.com>
2023-12-26 19:06:09 +0800

    1302. 层数最深叶子节点的和

--- src/main/java/com/github/morningwn/t1001_t1500/t1301_t1400/t1301_t1310/J1302.java
+++ src/main/java/com/github/morningwn/t1001_t1500/t1301_t1400/t1301_t1310/J1302.java
@@ -1,17 +1,27 @@
-package com.github.morningwn.t1001_t1500.t1301_t1400;
+package com.github.morningwn.t1001_t1500.t1301_t1400.t1301_t1310;
 
 import com.github.morningwn.prefab.TreeNode;
+import com.github.morningwn.tag.Difficulty;
+import com.github.morningwn.tag.Topic;
+import org.junit.Test;
 
 import java.util.Deque;
 import java.util.LinkedList;
 
 /**
  * <a href="https://leetcode.cn/problems/deepest-leaves-sum/">1302. 层数最深叶子节点的和</a>
+ * <ul>
+ * <li>{@link Difficulty#Medium}</li>
+ * <li>{@link Topic#Tree}</li>
+ * <li>{@link Topic#DepthFirstSearch}</li>
+ * <li>{@link Topic#BreadthFirstSearch}</li>
+ * <li>{@link Topic#BinaryTree}</li>
+ * </ul>
  *
  * @author morningwn
- * @create in 2022/8/17 17:04
+ * @date 2023-12-26 17:47:15
  */
-public class T1302 {
+public class J1302 {
 
     public int deepestLeavesSum(TreeNode root) {
         Deque<TreeNode> deque = new LinkedList<>();
@@ -36,4 +46,5 @@ public class T1302 {
 
         return res;
     }
+
 }
