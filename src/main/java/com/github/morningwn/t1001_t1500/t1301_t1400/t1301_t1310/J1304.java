1cfdb3c3354987ba091afea4be2a16fd7ea4a686
morningwn <morningwn@126.com>
2023-12-26 19:09:05 +0800

    1304. 和为零的 N 个不同整数

--- src/main/java/com/github/morningwn/t1001_t1500/t1301_t1400/t1301_t1310/J1304.java
+++ src/main/java/com/github/morningwn/t1001_t1500/t1301_t1400/t1301_t1310/J1304.java
@@ -1,13 +1,26 @@
-package com.github.morningwn.t1001_t1500.t1301_t1400;
+package com.github.morningwn.t1001_t1500.t1301_t1400.t1301_t1310;
 
 import com.github.morningwn.prefab.Out;
+import com.github.morningwn.tag.Difficulty;
+import com.github.morningwn.tag.Topic;
 import org.junit.Test;
 
-public class T1304 {
+
+/**
+ * <a href="https://leetcode.cn/problems/find-n-unique-integers-sum-up-to-zero/">1304. 和为零的 N 个不同整数</a>
+ * <ul>
+ * <li>{@link Difficulty#Easy}</li>
+ * <li>{@link Topic#Array}</li>
+ * <li>{@link Topic#Math}</li>
+ * </ul>
+ *
+ * @author morningwn
+ * @date 2020.03.22 19:52
+ */
+public class J1304 {
 
     @Test
     public void test() {
-
         for (int num : sumZero(4)) {
             Out.println(num + "\t ");
         }
@@ -29,5 +42,4 @@ public class T1304 {
 
         return nums;
     }
-
 }
