b8c0330363876aaa6bb2d2dc313fb28a2628c25e
morningwn <morningwn@126.com>
2024-07-03 14:22:11 +0800

    3099. 哈沙德数

---
+++ src/main/java/com/github/morningwn/t3001_t3500/t3001_t3100/t3091_t3100/JT3099.java
@@ -0,0 +1,22 @@
+package com.github.morningwn.t3001_t3500.t3001_t3100.t3091_t3100;
+
+/**
+ * <a href="https://leetcode.cn/problems/harshad-number/description/">3099. 哈沙德数</a>
+ *
+ * @author morningwn
+ * @date 2024/7/3 14:18
+ */
+public class JT3099 {
+
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int tmp = x;
+        int sum = 0;
+
+        while (tmp > 0) {
+            sum += tmp % 10;
+            tmp = tmp / 10;
+        }
+
+        return x % sum == 0 ? sum : -1;
+    }
+}
