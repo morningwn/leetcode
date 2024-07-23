a41dda2718aaefab4df906e1fe0d6c2b97074d69
morningwn <morningwn@126.com>
2024-07-23 18:10:13 +0800

    100. 相同的树

---
+++ src/main/java/com/github/morningwn/t0001_t0500/t0001_t0100/t0091_t0100/J0100.java
@@ -0,0 +1,19 @@
+package com.github.morningwn.t0001_t0500.t0001_t0100.t0091_t0100;
+
+import com.github.morningwn.prefab.TreeNode;
+
+/**
+ * <a href="https://leetcode.cn/problems/same-tree/">100. 相同的树</a>
+ */
+public class J0100 {
+    
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
