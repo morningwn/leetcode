423812803b2ba412e85a39384943780a5594ca31
morningwn <morningwn@126.com>
2024-07-23 18:14:57 +0800

    96. 不同的二叉搜索树

--- src/main/java/com/github/morningwn/t0001_t0500/t0001_t0100/t0091_t0100/J0096.java
+++ src/main/java/com/github/morningwn/t0001_t0500/t0001_t0100/t0091_t0100/J0096.java
@@ -1,10 +1,12 @@
 package com.github.morningwn.t0001_t0500.t0001_t0100.t0091_t0100;
 
 /**
+ * <a href="https://leetcode.cn/problems/unique-binary-search-trees/">96. 不同的二叉搜索树</a>
+ *
  * @author morningwn
  * @date Created in 2020/7/15 15:57
  */
-public class T0096 {
+public class J0096 {
 
     public int numTrees(int n) {
         int[] res = new int[n + 1];
