af4b09ec467348633b8ae53699b5ed67ffa18691
morningwn <morningwn@126.com>
2025-02-26 12:00:52 +0800

    1480. 一维数组的动态和

--- src/main/java/com/github/morningwn/t1001_t1500/t1401_t1500/t1471_t1480/J1480.java
+++ src/main/java/com/github/morningwn/t1001_t1500/t1401_t1500/t1471_t1480/J1480.java
@@ -1,18 +1,18 @@
 package com.github.morningwn.t1001_t1500.t1401_t1500.t1471_t1480;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
+ * <a href="https://leetcode.cn/problems/running-sum-of-1d-array/">1480. 一维数组的动态和</a>
  * @author morningwn
- * @description: 一维数组的动态和
  * @date Created in 2020/7/19 18:02
- * @version: 1.0
  */
-public class T1480 {
+public class J1480 {
 
     @Test
     public void test() {
-
+        Assert.assertArrayEquals(new int[]{1, 3, 6, 10}, runningSum(new int[]{1, 2, 3, 4}));
     }
 
     public int[] runningSum(int[] nums) {
