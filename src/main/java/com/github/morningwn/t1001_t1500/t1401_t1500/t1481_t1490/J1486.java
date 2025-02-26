e23c4fdc1a67c59797c305829d603cc78e138ecf
morningwn <morningwn@126.com>
2025-02-26 14:15:43 +0800

    1486. 数组异或操作

--- src/main/java/com/github/morningwn/t1001_t1500/t1401_t1500/t1481_t1490/J1486.java
+++ src/main/java/com/github/morningwn/t1001_t1500/t1401_t1500/t1481_t1490/J1486.java
@@ -1,11 +1,12 @@
 package com.github.morningwn.t1001_t1500.t1401_t1500.t1481_t1490;
 
 /**
+ * <a href="https://leetcode.cn/problems/xor-operation-in-an-array/>1486. 数组异或操作</a>
+ *
  * @author morningwn
- * @description: 数组异或操作
  * @date Created in 2020/7/20 17:54
  */
-public class T1486 {
+public class J1486 {
 
     public int xorOperation(int n, int start) {
         int res = start;
