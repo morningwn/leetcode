fd50a3cea4f295a623284132639cbf8cab8dc35d
morningwn <morningwn@126.com>
2025-02-28 16:44:09 +0800

    2379. 得到 K 个黑块的最少涂色次数

--- src/main/java/com/github/morningwn/t2001_t2500/t2301_t2400/t2371_t2380/J2379.java
+++ src/main/java/com/github/morningwn/t2001_t2500/t2301_t2400/t2371_t2380/J2379.java
@@ -3,15 +3,13 @@ package com.github.morningwn.t2001_t2500.t2301_t2400.t2371_t2380;
 import com.github.morningwn.prefab.Out;
 import org.junit.Test;
 
-import java.security.PublicKey;
-
 /**
  * <a href="https://leetcode.cn/problems/minimum-recolors-to-get-k-consecutive-black-blocks/">2379. 得到 K 个黑块的最少涂色次数</a>
  *
  * @author morningwn
  * @date Created in 2023/3/9 21:23
  */
-public class T2379 {
+public class J2379 {
 
     @Test
     public void test() {
