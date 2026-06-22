ff0e844d817ff6249f9e25f2dd79a4222a48fb0b
morningwn <morningwn@126.com>
2026-06-22 20:37:54 +0800

    3612. 用特殊操作处理字符串 I

---
+++ src/main/java/com/github/morningwn/t3001_t3500/t3601_t3700/t3611_t3620/J3612.java
@@ -0,0 +1,30 @@
+package com.github.morningwn.t3001_t3500.t3601_t3700.t3611_t3620;
+
+/**
+ * <a href="https://leetcode.cn/problems/process-string-with-special-operations-i">3612. 用特殊操作处理字符串 I</a>
+ *
+ * @author morningwn
+ * @date 2026-06-22 20:30
+ */
+public class J3612 {
+
+    public String processStr(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (c == '*') {
+                if (!sb.isEmpty()) {
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+            } else if (c == '#') {
+                sb.append(sb);
+            } else if (c == '%') {
+                sb.reverse();
+            } else {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+}
