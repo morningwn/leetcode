3c50fff11797bcd1f4f92f5735a6b506ee820854
morningwn <morningwn@126.com>
2026-07-15 10:34:52 +0800

    1389. 按既定顺序创建目标数组

--- src/main/java/com/github/morningwn/t1001_t1500/t1301_t1400/t1381_t1390/J1389.java
+++ src/main/java/com/github/morningwn/t1001_t1500/t1301_t1400/t1381_t1390/J1389.java
@@ -1,19 +1,10 @@
 package com.github.morningwn.t1001_t1500.t1301_t1400.t1381_t1390;
 
-import com.github.morningwn.tag.Difficulty;
-import com.github.morningwn.tag.Topic;
-
 import java.util.ArrayList;
 import java.util.List;
 
-
 /**
  * <a href="https://leetcode.cn/problems/create-target-array-in-the-given-order/">1389. 按既定顺序创建目标数组</a>
- * <ul>
- * <li>{@link Difficulty#Easy}</li>
- * <li>{@link Topic#Array}</li>
- * <li>{@link Topic#Simulation}</li>
- * </ul>
  *
  * @author morningwn
  * @date 2020/7/21 16:15
