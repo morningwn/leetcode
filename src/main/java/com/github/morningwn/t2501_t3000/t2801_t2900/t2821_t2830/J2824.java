7052352d928cc68c6dd699974131ef311e3333b2
morningwn <morningwn@126.com>
2026-07-15 10:43:53 +0800

    2824. 统计和小于目标的下标对数目

--- src/main/java/com/github/morningwn/t2501_t3000/t2801_t2900/t2821_t2830/J2824.java
+++ src/main/java/com/github/morningwn/t2501_t3000/t2801_t2900/t2821_t2830/J2824.java
@@ -1,19 +1,11 @@
 package com.github.morningwn.t2501_t3000.t2801_t2900.t2821_t2830;
 
-import com.github.morningwn.tag.Difficulty;
-import com.github.morningwn.tag.Topic;
 import org.junit.Test;
 
 import java.util.List;
 
 /**
  * <a href="https://leetcode.cn/problems/count-pairs-whose-sum-is-less-than-target/">2824. 统计和小于目标的下标对数目</a>
- * <ul>
- * <li>{@link Difficulty#Easy}</li>
- * <li>{@link Topic#Array}</li>
- * <li>{@link Topic#TwoPointers}</li>
- * <li>{@link Topic#Sorting}</li>
- * </ul>
  *
  * @author morningwn
  * @date 2023-11-24 10:15:24
