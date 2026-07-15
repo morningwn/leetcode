ba086284d351277f453f360217af79f35eb0b232
morningwn <morningwn@126.com>
2026-07-15 10:33:57 +0800

    3658. 奇数和与偶数和的最大公约数

---
+++ src/main/java/com/github/morningwn/t3501_t4000/t3601_t3700/t3651_t3660/J3658.java
@@ -0,0 +1,13 @@
+package com.github.morningwn.t3501_t4000.t3601_t3700.t3651_t3660;
+
+/**
+ * <a href="https://leetcode.cn/problems/gcd-of-odd-and-even-sums">3658. 奇数和与偶数和的最大公约数</a>
+ *
+ * @author morningwn
+ * @date 2026-07-15 10:33
+ */
+public class J3658 {
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
