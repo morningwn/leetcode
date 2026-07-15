722380d828b64c04b7c83c65e3002f43c8fdd0b0
morningwn <morningwn@126.com>
2026-07-15 10:42:46 +0800

    1944. 队列中可以看到的人数

--- src/main/java/com/github/morningwn/t1501_t2000/t1901_t2000/t1941_t1950/J1944.java
+++ src/main/java/com/github/morningwn/t1501_t2000/t1901_t2000/t1941_t1950/J1944.java
@@ -1,23 +1,14 @@
 package com.github.morningwn.t1501_t2000.t1901_t2000.t1941_t1950;
 
 import com.github.morningwn.prefab.Out;
-import com.github.morningwn.tag.Difficulty;
-import com.github.morningwn.tag.Topic;
 import org.junit.Test;
 
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Random;
 
-
 /**
  * <a href="https://leetcode.cn/problems/number-of-visible-people-in-a-queue/">1944. 队列中可以看到的人数</a>
- * <ul>
- * <li>{@link Difficulty#Hard}</li>
- * <li>{@link Topic#Stack}</li>
- * <li>{@link Topic#Array}</li>
- * <li>{@link Topic#MonotonicStack}</li>
- * </ul>
  *
  * @author morningwn
  * @date 2024-01-05 16:50:29
