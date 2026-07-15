bbdd93ed1ab580875c60501394798ce2708e5df7
morningwn <morningwn@126.com>
2026-07-15 10:40:25 +0800

    1276. 不浪费原料的汉堡制作方案

--- src/main/java/com/github/morningwn/t1001_t1500/t1201_t1300/t1271_t1280/J1276.java
+++ src/main/java/com/github/morningwn/t1001_t1500/t1201_t1300/t1271_t1280/J1276.java
@@ -1,7 +1,5 @@
 package com.github.morningwn.t1001_t1500.t1201_t1300.t1271_t1280;
 
-import com.github.morningwn.tag.Difficulty;
-import com.github.morningwn.tag.Topic;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -9,10 +7,6 @@ import java.util.List;
 
 /**
  * <a href="https://leetcode.cn/problems/number-of-burgers-with-no-waste-of-ingredients/">1276. 不浪费原料的汉堡制作方案</a>
- * <ul>
- * <li>{@link Difficulty#Medium}</li>
- * <li>{@link Topic#Math}</li>
- * </ul>
  *
  * @author morningwn
  * @date 2023-12-25 11:06:49
@@ -23,7 +17,6 @@ public class J1276 {
     public void test() {
 
     }
-
 
     public List<Integer> numOfBurgers(int tomatoSlices, int cheeseSlices) {
         List<Integer> nums = new ArrayList<>();
