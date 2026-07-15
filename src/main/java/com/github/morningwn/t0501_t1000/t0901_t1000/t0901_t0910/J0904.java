fbd684ec13d7ee03f864c14c1ad90421665824bf
morningwn <morningwn@126.com>
2026-07-15 10:40:39 +0800

    904. 水果成篮

--- src/main/java/com/github/morningwn/t0501_t1000/t0901_t1000/t0901_t0910/J0904.java
+++ src/main/java/com/github/morningwn/t0501_t1000/t0901_t1000/t0901_t0910/J0904.java
@@ -1,19 +1,10 @@
 package com.github.morningwn.t0501_t1000.t0901_t1000.t0901_t0910;
 
 import com.github.morningwn.prefab.Out;
-import com.github.morningwn.tag.Difficulty;
-import com.github.morningwn.tag.Topic;
 import org.junit.Test;
-
 
 /**
  * <a href="https://leetcode.cn/problems/fruit-into-baskets/">904. 水果成篮</a>
- * <ul>
- * <li>{@link Difficulty#Medium}</li>
- * <li>{@link Topic#Array}</li>
- * <li>{@link Topic#HashTable}</li>
- * <li>{@link Topic#SlidingWindow}</li>
- * </ul>
  *
  * @author morningwn
  * @date 2023-11-27 12:03:17
