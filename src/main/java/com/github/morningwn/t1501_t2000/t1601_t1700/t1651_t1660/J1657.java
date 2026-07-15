e610d239b3c3c5631f2ef1b6717be581a14b5124
morningwn <morningwn@126.com>
2026-07-15 10:42:35 +0800

    1657. 确定两个字符串是否接近

--- src/main/java/com/github/morningwn/t1501_t2000/t1601_t1700/t1651_t1660/J1657.java
+++ src/main/java/com/github/morningwn/t1501_t2000/t1601_t1700/t1651_t1660/J1657.java
@@ -1,32 +1,22 @@
 package com.github.morningwn.t1501_t2000.t1601_t1700.t1651_t1660;
 
-import com.github.morningwn.tag.Difficulty;
-import com.github.morningwn.tag.Topic;
 import org.junit.Test;
 
-
 /**
-* <a href="https://leetcode.cn/problems/determine-if-two-strings-are-close/">1,657. 确定两个字符串是否接近</a>
-* <ul>
-* <li>{@link Difficulty#Medium}</li>
-* <li>{@link Topic#HashTable}</li>
-* <li>{@link Topic#String}</li>
-* <li>{@link Topic#Sorting}</li>
-* </ul>
-*
-* @author morningwn
-* @date 2023-11-30 17:48:56
-*/
+ * <a href="https://leetcode.cn/problems/determine-if-two-strings-are-close/">1657. 确定两个字符串是否接近</a>
+ *
+ * @author morningwn
+ * @date 2023-11-30 17:48:56
+ */
 public class J1657 {
 
-@Test
-public void test() {
+    @Test
+    public void test() {
 
-}
-
+    }
 
     public boolean closeStrings(String word1, String word2) {
 
-    return false;
+        return false;
     }
 }
