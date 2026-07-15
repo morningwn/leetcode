d4fca0f313e4169e11264182a86856067b4a744a
morningwn <morningwn@126.com>
2026-07-15 10:40:12 +0800

    1038. 从二叉搜索树到更大和树

--- src/main/java/com/github/morningwn/t1001_t1500/t1001_t1100/t1031_t1040/J1038.java
+++ src/main/java/com/github/morningwn/t1001_t1500/t1001_t1100/t1031_t1040/J1038.java
@@ -1,19 +1,9 @@
 package com.github.morningwn.t1001_t1500.t1001_t1100.t1031_t1040;
 
 import com.github.morningwn.prefab.TreeNode;
-import com.github.morningwn.tag.Difficulty;
-import com.github.morningwn.tag.Topic;
-import org.junit.Test;
 
 /**
  * <a href="https://leetcode.cn/problems/binary-search-tree-to-greater-sum-tree/">1038. 从二叉搜索树到更大和树</a>
- * <ul>
- * <li>{@link Difficulty#Medium}</li>
- * <li>{@link Topic#Tree}</li>
- * <li>{@link Topic#DepthFirstSearch}</li>
- * <li>{@link Topic#BinarySearchTree}</li>
- * <li>{@link Topic#BinaryTree}</li>
- * </ul>
  *
  * @author morningwn
  * @date 2023-12-04 17:19:12
